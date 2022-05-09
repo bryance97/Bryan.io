d5b6f10a23bf25ae33d8f8be2b286296dd855d35
bryance97 <bryancifuentesescobar@gmail.com>
2022-05-09 13:46:49 -0400

    final touches on side bar

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -4,6 +4,7 @@ import LogoS from '../../assets/images/logo-s.png'
 import LogoSubtitle from '../../assets/images/logo_sub.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHome, faEnvelope, faUser } from '@fortawesome/free-solid-svg-icons'
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
 
 const Sidebar = () => (
   <div className="nav-bar">
@@ -19,7 +20,7 @@ const Sidebar = () => (
       <NavLink
         exact="true"
         activeclassname="active"
-        classname="about-link"
+        className="about-link"
         to="/about"
       >
         <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
@@ -28,12 +29,41 @@ const Sidebar = () => (
       <NavLink
         exact="true"
         activeclassname="active"
-        classname="contact-link"
+        className="contact-link"
         to="/contact"
       >
         <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
       </NavLink>
     </nav>
+    <ul>
+      <li>
+      <a
+        target="_blank"
+        rel="noreferrer"
+        href="https://www.linkedin.com/in/bryan-cifuentes-escobar/"
+        >
+          <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+        </a>
+      </li>
+      <li>
+      <a
+        target="_blank"
+        rel="noreferrer"
+        href="https://github.com/bryance97"
+        >
+          <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+        </a>
+      </li>
+      <li>
+      <a
+        target="_blank"
+        rel="noreferrer"
+        href="https://www.linkedin.com/in/bryan-cifuentes-escobar/"
+        >
+          <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+        </a>
+      </li>
+    </ul>
   </div>
 )
 
